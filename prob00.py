0db47e673ee9fdf1d616e13eac4c68f6dc1cf6ef
IdiotStinker <saltylightning564@gmail.com>
2025-02-01 08:58:17 -0800

    slave = slav

---
+++ prob00.py
@@ -0,0 +1,2 @@
+print("We are pleased to announce the launch of our new game.")
+print("It is going to dominate the... hang on I am being handed a note...")
---
+++ prob01.py
@@ -0,0 +1,4 @@
+with open("input.txt", "r") as f:
+    words = f.read()
+word = words.split("\n")[0]
+print(f"Retraction: Did I say that out loud? I apologize, {word}. While you are a meatbag, I suppose I should not call you such.")
